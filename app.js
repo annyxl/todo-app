4ad53943163223132543fb6f465df70a14536f3e
Anny Wang <kingdom2069@126.com>
2024-08-02 22:14:25 -0600

    add POST /todo

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// add 4 routes at /todo: get, post, put, delete
+
+// const todo = app.route('/todo')
+
+//get
+
+//post
+app.post('/todo', (req, res) => {
+  // console.log(req.body)
+  res.send('Post works!')
+})
+
+//put
+
+//delete
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}. http://localhost:${port}`)
 })
