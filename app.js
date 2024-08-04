5427c01d8cd818ee0afa8b9294169c4ed0fb0f4d
Anny Wang <kingdom2069@126.com>
2024-08-03 20:36:15 -0600

    add other todo endpoints

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const todoRouter = require('./src/routes/todo')
+
+app.use(express.json()) // puts on req.body
+app.use('/api', todoRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-// add 4 routes at /todo: get, post, put, delete
-
-// const todo = app.route('/todo')
-
-//get
-
-//post
-app.post('/todo', (req, res) => {
-  // console.log(req.body)
-  res.send('Post works!')
-})
-
-//put
-
-//delete
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}. http://localhost:${port}`)
