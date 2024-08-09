4ff416a80a06106c24e678831aac5fd8d99bb833
Anny Wang <kingdom2069@126.com>
2024-08-09 16:32:47 -0600

    use components and clean up css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from 'react'
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
 import './App.css'
 
 function App() {
+  const [todos, setTodos] = useState([])
+
+  const updateTodos = () => {
+    fetch('http://localhost:3000/api/todos')
+      .then(res => res.json())
+      .then(res => setTodos(res))
+  }
+
+  useEffect(() => {
+    updateTodos()
+  }, [])
   
   return (
-    <>
-      <h1>Todo App</h1>
-    </>
+    <main>
+      <h1>Todo List</h1>
+      <AddTodo updateTodos={updateTodos}/>
+      <br></br>
+      <Todos todos={todos} updateTodos={updateTodos} />
+    </main>
   )
 }
 
